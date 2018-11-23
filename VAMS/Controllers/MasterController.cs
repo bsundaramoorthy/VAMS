a5f85243a9948ca9f23eef1ac638a1a587f32887
unknown <Administrator@u4s.com>
2018-11-23 12:42:49 +0530

    First commit

---
+++ VAMS/Controllers/MasterController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VAMS.Common;
+
+namespace VAMS.Controllers
+{
+    public class MasterController : Controller
+    {
+
+       public ActionResult Department()
+        {
+            if (SessionHelper.Current.UserID == 0)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                Common.SessionHelper.Current.CurrentPageName = "Department";
+                ViewBag.Lang = SessionHelper.Current.Lang;
+                return View();
+            }
+        }
+       public ActionResult Designation()
+        {
+            if (SessionHelper.Current.UserID == 0)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                Common.SessionHelper.Current.CurrentPageName = "Designation";
+                ViewBag.Lang = SessionHelper.Current.Lang;
+                return View();
+            }
+        }
+
+        public ActionResult Grade()
+        {
+            if (SessionHelper.Current.UserID == 0)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                Common.SessionHelper.Current.CurrentPageName = "Grade";
+                ViewBag.Lang =SessionHelper.Current.Lang;
+                return View();
+            }
+        }
+
+    }
+}
---
+++ VAMS/Scripts/Master/Department.js
@@ -0,0 +1,221 @@
+﻿
+$(document).ready(function () {
+    LoadCompanyDropdown();
+    LoadDepartment();
+});
+
+function LocationChange() {
+    ClearMsg();
+    LoadDepartment();
+}
+
+function LoadDepartment() {
+    var table = '';
+    var tr = '';
+    var CompanyID = $('#ddlCompany').val();
+    $.ajax({
+        type: "GET",
+        url: baseUrl + "API/Department/GetAll",
+        data: { "ID": 0, "CompanyID": CompanyID },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: false,
+        success: function (response) {
+            if (response.length > 0) {
+                table = table + '<table class="display nowrap table-responsive" id="example" style="width:100%"><thead><th>' + SerialNoLang + '<th>' + DepartmentLang + '</th><th>' + ShortNameLang + '</th><th>' + EditLang + '</th><th>' + DeleteLang + '</th></thead><tbody>'
+                $.each(response, function (key, val) {
+                    tr = tr + '<tr><td>' + parseInt(parseInt(key) + parseInt(1)) + '</td >';
+                    tr = tr + '<td>' + val.DepartmentName + '</td >';
+                    tr = tr + '<td>' + val.DepartmentShortName + '</td>';
+                    tr = tr + '<td ><a class="btn btn-primary" onclick="Edit(' + val.DepartmentID + ')"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>';
+                    tr = tr + '<td ><a class="btn  btn-danger" onclick="Delete(' + val.DepartmentID + ',\'' + val.DepartmentName + '\')"><i class="fa fa-trash" aria-hidden="true"></i></td></tr>';
+                });
+                table = table + tr + '</tbody></table>';
+                $('#dvTable').html('');
+                $('#dvTable').html(table);
+                $('#example').DataTable({
+                    "aoColumnDefs": [
+                        { 'bSortable': false, 'aTargets': [3, 4] }
+                    ],
+                    "scrollX": true,
+                    "scrollY": true,
+                    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+                });
+            }
+            else {
+                table = table + '<table class="display nowrap table-responsive" id="example" style="width:100%"><thead><th>' + SerialNoLang + '<th>' + DepartmentLang + '</th><th>' + ShortNameLang + '</th><th>' + EditLang + '</th><th>' + DeleteLang + '</th></thead><tbody>'
+                table = table + tr + '</tbody></table>';
+                $('#dvTable').html('');
+                $('#dvTable').html(table);
+                $('#example').DataTable({
+                    "aoColumnDefs": [
+                        { 'bSortable': false, 'aTargets': [3, 4] }
+                    ],
+                    //"scrollX": true,
+                    "scrollY": true,
+                    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+                });
+            }
+
+        },
+        failure: function (response) {
+            console.log('failure' + response.responseText);
+        },
+        error: function (response) {
+            console.log('error' + response.responseText);
+        }
+    });
+}
+
+function showModal() {
+    ClearMsg();
+    ClearData();
+    $('#hdnDepartmentID').val(0);
+    $('#myModal').modal('show');
+
+}
+
+function ClearData() {
+    $('#hdnDepartmentID').val(0);
+    $('#txtDepartment').val('');
+    $('#txtDepartmentShort').val('');
+    AssignSuccess("txtDepartment");
+    AssignSuccess("txtDepartmentShort");
+}
+
+function Save() {
+    ClearMsg();
+    ValidateFields('txtDepartment', 'txt');
+    ValidateFields('txtDepartmentShort', 'txt');
+
+    if ($('#txtDepartment').val() == '') {
+        return;
+    }
+    if ($('#txtDepartmentShort').val() == '') {
+        return;
+    }
+
+    var Department = new Object();
+    Department.DepartmentID = $('#hdnDepartmentID').val();
+    Department.DepartmentName = $('#txtDepartment').val();
+    Department.DepartmentShortName = $('#txtDepartmentShort').val();
+    Department.CompanyID = $('#ddlCompany').val();
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "API/Department/InsertUpdateDept",
+        data: JSON.stringify(Department),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async:false,
+        success: function (response) {
+            if (response == -1) {
+                Content = ContentMaking(DepartmentLang, Department.DepartmentName, NotSaveLang);
+                ShowErrorMsg(Content);
+            }
+            else if (response == 1) {
+
+                if ($('#hdnDepartmentID').val() == "0") {
+                    Content = ContentMaking(DepartmentLang, Department.DepartmentName, SaveMsgLang);
+                    ShowSuccessMsg(Content);
+                }
+                if ($('#hdnDepartmentID').val() != "0") {
+                    Content = ContentMaking(DepartmentLang, Department.DepartmentName, UpdateMsgLang);
+                    ShowSuccessMsg(Content);
+                }
+            
+                LoadDepartment();
+                ClearData();
+                $('#myModal').modal('hide');
+            }
+            else {
+                Content = ContentMaking(DepartmentLang,Department.DepartmentName, AlreadyExistMsgLang);
+                ShowErrorMsg(Content);
+            }
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+}
+
+function Edit(ID) {
+    var CompanyID = $('#ddlCompany').val();
+    $.ajax({
+        type: "GET",
+        url: baseUrl + "API/Department/GetAll",
+        data: { "ID": ID, "CompanyID": CompanyID },
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: false,
+        success: function (response) {
+
+            if (response.length > 0) {
+                $('#txtDepartment').val(response[0].DepartmentName);
+                $('#txtDepartmentShort').val(response[0].DepartmentShortName);
+                $('#hdnDepartmentID').val(response[0].DepartmentID);
+
+                ValidateFields('txtDepartment', 'txt');
+                ValidateFields('txtDepartmentShort', 'nddl');
+                $('#myModal').modal('show');
+            }
+        },
+        failure: function (response) {
+            console.log('failure' + response.responseText);
+        },
+        error: function (response) {
+            console.log('error' + response.responseText);
+        }
+    });
+    ClearMsg();
+}
+
+function Delete(ID, Building) {
+    bootbox.confirm({
+        title: "Department",
+        message: "Do u want to delete this?",
+        buttons: {
+            cancel: {
+                label: '<i class="fa fa-times"></i>Cancel'
+            },
+            confirm: {
+                label: '<i class="fa fa-check"></i> Confirm'
+            }
+        },
+        callback: function (result) {
+            if (result == true) {
+                ClearMsg();
+                $.ajax({
+                    type: "GET",
+                    url: baseUrl + "API/Department/Delete",
+                    data: { "ID": ID },
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function (response) {
+                        if (response == 1) {
+                            Content = ContentMaking(BuildingLang, Building, DeleteMsgLang);
+                            ShowSuccessMsg(Content);
+                            LoadDepartment();
+                            ClearData();
+                        }
+                        else {
+                            //Content = ContentMaking(BuildingLang, Building, AlreadyUsedMsgLang);
+                            //ShowErrorMsgCommon(Content);
+                            //ShowSuccessMsg("Department already used");
+                        }
+                    },
+                    failure: function (response) {
+                        console.log('failure' + response.responseText);
+                    },
+                    error: function (response) {
+                        console.log('error' + response.responseText);
+                    }
+                });
+            }
+        }
+    });
+}
+
+
